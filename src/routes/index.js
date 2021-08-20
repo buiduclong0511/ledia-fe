ea331d4c4553ac3073d6ac1f7b92b877ae66c183
Bùi Đức Long <buiduclong0511@gmail.com>
2021-08-20 08:32:23 +0700

    fix route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import { Home, Upload } from "src/pages";
 
-export const PATH_HOME = "/";
-export const PATH_UPLOAD = "/upload";
+export const PATH_HOME = "/ledia-fe/";
+export const PATH_UPLOAD = "/ledia-fe/upload";
 
 export const router = [
     { path: PATH_HOME, component: Home },
